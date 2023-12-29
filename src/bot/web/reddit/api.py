d476d3b58abb6c1f163566622c7b7bb15d957529
CBoYXD <python.rust.cpp@gmail.com>
2023-12-29 15:22:13 +0200

    Add new methods to RedditAPI

--- src/bot/web/reddit/api.py
+++ src/bot/web/reddit/api.py
@@ -1,18 +1,58 @@
 from typing import Any, Optional
 
 import aiohttp
-
-from .core import Post, RedditAPIException, SubReddit
+from datetime import datetime
+from .core import RedditAPIException, Post, SubReddit, Redditor
 
 
 class RedditAPI:
-    def __util_get_posts(
+    def __tool_get_redditors(
+        self, user_redditors: list[dict[str, Any]]
+    ) -> list[Redditor]:
+        sort_redditors = []
+        for redditor in user_redditors:
+            name = redditor["name"]
+            sort_redditors.append(
+                Redditor(
+                    name=name,
+                    id=redditor["id"],
+                    link=f"https://www.reddit.com/user/{name}",
+                    comment_karma=redditor["comment_karma"],
+                    link_karma=redditor["link_karma"],
+                    created_utc=datetime.fromtimestamp(redditor["created_utc"]),
+                    accept_chats=redditor["accept_chats"],
+                    accept_pms=redditor["accept_pms"],
+                    accept_followers=redditor["accept_followers"],
+                    verified=redditor["verified"],
+                )
+            )
+
+    def __tool_get_subreddits(
+        self, user_subreddits: list[dict[str, Any]]
+    ) -> list[SubReddit]:
+        sort_subreddits = []
+        for subreddit in user_subreddits:
+            name = subreddit["display_name"]
+            sort_subreddits.append(
+                SubReddit(
+                    name=name,
+                    link=f"https://www.reddit.com/r/{name}",
+                    title=subreddit["title"],
+                    lang=subreddit["lang"],
+                    id=subreddit["id"],
+                    type=subreddit["subreddit_type"],
+                    over_18=subreddit["over18"],
+                    created_at=datetime.fromtimestamp(subreddit["created_utc"]),
+                )
+            )
+
+    def __tool_get_posts(
         self,
         user_posts: list[dict[str, Any]],
-        subreddit: str,
-        link: str,
-        post_type: str,
+        subreddit: Optional[str] = None,
+        post_type: Optional[str] = None,
     ) -> list[Post]:
+        subreddit = subreddit if not None else user_posts["subreddit"]
         posts = []
         for post in user_posts:
             posts.append(
@@ -22,19 +62,18 @@ class RedditAPI:
                     title=post["title"],
                     author=post["author"],
                     post_type=post_type,
-                    subreddit=SubReddit(
-                        name=subreddit,
-                        id=post["subreddit_id"],
-                        subscribers=post["subreddit_subscribers"],
-                        tag="#" + subreddit,
-                    ),
-                    created_at=post["created_utc"],
+                    subreddit_name=subreddit,
+                    subreddit_id=post["subreddit_id"],
+                    subreddit_subscribers=post["subreddit_subscribers"],
+                    subreddit_tag="#" + subreddit,
+                    created_at=datetime.fromtimestamp(post["created_utc"]),
                     upvote_ratio=post["upvote_ratio"],
                     comments_num=post["num_comments"],
                     score=post["score"],
                     is_video=post["is_video"],
                     media_only=post["media_only"],
                     over_18=post["over_18"],
+                    has_spoiler=post["spoiler"],
                 )
             )
         return posts
@@ -42,32 +81,44 @@ class RedditAPI:
     async def get_posts(
         self,
         subreddit: str,
-        limit: int = 9,
         post_type: str = "new",
+        limit: Optional[int] = None,
         id: Optional[str] = None,
     ) -> list[Post]:
         if post_type not in ("new", "hot", "top", "rising"):
             raise RedditAPIException("Wrong param post_type")
-        link = f"https://www.reddit.com/r/{subreddit}/{post_type}.json?"
+        if limit is not None and (0 > limit or limit > 100):
+            raise RedditAPIException("Param limit can only be from 0 to 100 inclusive")
+        if (limit is not None) and (id is not None):
+            raise RedditAPIException(
+                "Params limit and id cannot be true at the same time "
+                + "(one of the parameters must be None)"
+            )
+
         async with aiohttp.ClientSession() as ses:
-            async with ses.get(link) as res:
+            async with ses.get(
+                "{link}r/{subreddit}/{post_type}.json?&{limit}".format(
+                    link="https://www.reddit.com/",
+                    subreddit=subreddit,
+                    post_type=post_type,
+                    limit=limit,
+                )
+            ) as res:
                 json = await res.json()
                 reddit_posts = [post["data"] for post in json["data"]["children"]]
                 if not id:
-                    return self.__util_get_posts(
-                        user_posts=reddit_posts[: limit - 1],
+                    return self.__tool_get_posts(
+                        user_posts=reddit_posts,
                         subreddit=subreddit,
-                        link=link,
                         post_type=post_type,
                     )
                 else:
                     for post in reddit_posts:
                         if post["id"] == id:
                             index = reddit_posts.index(post)
-                    return self.__util_get_posts(
+                    return self.__tool_get_posts(
                         user_posts=reddit_posts[:index],
                         subreddit=subreddit,
-                        link=link,
                         post_type=post_type,
                     )
 
@@ -95,11 +146,61 @@ class RedditAPI:
                 else:
                     return False
 
-    async def check_subreddits(self, subreddits: list | tuple) -> list[tuple[str, bool]]:
+    async def check_subreddits(
+        self, subreddits: list | tuple
+    ) -> list[tuple[str, bool]]:
         return [
             (subreddit, await self.check_subreddit(subreddit))
             for subreddit in subreddits
         ]
+
+    async def search_request(
+        self,
+        request: str,
+        search_type: str = "link",
+        limit: Optional[int] = None,
+        sort_type: Optional[str] = None,
+        time: Optional[str] = None,
+    ) -> list[Post] | None:
+        if search_type not in ("link", "sr", "user"):
+            raise RedditAPIException("Wrong search_type param")
+        if limit is not None and (0 > limit or limit > 100):
+            raise RedditAPIException("Param limit can only be from 0 to 100 inclusive")
+        if search_type == "link" and (
+            sort_type not in ("relevance", "hot", "new", "comments", "top")
+        ):
+            raise RedditAPIException("Wrong sort_type for search_type 'link'")
+        elif search_type != "link" and (time or sort_type):
+            raise RedditAPIException(
+                "Params time and sort_type can be only None, "
+                "if param search_type != link"
+            )
+        async with aiohttp.ClientSession() as ses:
+            async with ses.get(
+                "{link}q={request}&{limit}&{type}&{sort}&{time}".format(
+                    link="https://www.reddit.com/search/.json?",
+                    request=request,
+                    limit=limit,
+                    type=search_type,
+                    sort=sort_type,
+                    time=time,
+                )
+            ) as res:
+                json = await res.json()
+                if json.get("error") is None:
+                    data = [post["data"] for post in json["data"]["children"]]
+                    if search_type == "link":
+                        return self.__tool_get_posts(
+                            user_posts=data,
+                            post_type=sort_type,
+                        )
+                    elif search_type == "sr":
+                        return self.__tool_get_subreddits(user_subreddits=data)
+                    else:
+                        return self.__tool_get_redditors(user_redditors=data)
+
+                else:
+                    return None
 
     def __str__(self) -> str:  # for tests
         return "Reddit API Object"
--- src/bot/web/reddit/core.py
+++ src/bot/web/reddit/core.py
@@ -7,9 +7,72 @@ from aiogram.utils.formatting import Bold, HashTag, TextLink
 @dataclass
 class SubReddit:
     name: str
+    title: str
+    link: str
+    lang: str
     id: str
-    tag: str
-    subscribers: int
+    type: str
+    over_18: bool
+    created_at: datetime
+
+    def __str__(self) -> str:
+        over_18 = (
+            ""
+            if self.over_18 is False
+            else Bold(f"🔞 Over 18 🔞: {self.over_18}\n\n").as_html()
+        )
+        return (
+            Bold("👥Subreddit👥: ").as_html()
+            + TextLink(self.name, self.link).as_html()
+            + "\n\n"
+            + Bold(f"🆔: {self.id}").as_html()
+            + "\n\n"
+            + Bold(f"🔍 Title 🔍: {self.title}")
+            + Bold(f"🗣 Language 💬 {self.language}").as_html()
+            + "\n\n"
+            + Bold(f"🧩 Type 🧩: {self.type}")
+            + "\n\n"
+            + over_18
+            + Bold(f"⏳ Created UTC ⏳: {str(self.created_utc)}").as_html()
+            + "\n\n"
+        )
+
+
+@dataclass
+class Redditor:
+    name: str
+    id: str
+    link: str
+    comment_karma: int
+    link_karma: int
+    accept_chats: bool
+    accept_pms: bool
+    accept_followers: bool
+    verified: bool
+    created_utc: datetime
+
+    def __str__(self) -> str:
+        return (
+            Bold("👤 User 👤: ").as_html()
+            + TextLink(self.name, self.link).as_html()
+            + "\n\n"
+            + Bold(f"🆔: {self.id}").as_html()
+            + "\n\n"
+            + Bold(f"🔮 Comment Karma 💬: {self.comment_karma}").as_html()
+            + "\n\n"
+            + Bold(f"📤 Link Karma 🔮: {self.link_karma}").as_html()
+            + "\n\n"
+            + Bold(f"✅ Accept Chats 💭: {self.accept_chats}").as_html()
+            + "\n\n"
+            + Bold(f"✅ Accept PMs ✍️: {self.accept_pms}").as_html()
+            + "\n\n"
+            + Bold(f"✅ Accept Followers 🤝: {self.accept_followers}").as_html()
+            + "\n\n"
+            + Bold(f"✅ Verified 🔰: {self.verified}").as_html()
+            + "\n\n"
+            + Bold(f"⏳ Created UTC ⏳: {str(self.created_utc)}").as_html()
+            + "\n\n"
+        )
 
 
 @dataclass
@@ -19,14 +82,18 @@ class Post:
     title: str
     author: str
     post_type: str  # Hot, New, Best, Top, Rising, etc.
-    subreddit: SubReddit
-    upvote_ratio: float
-    created_at: int
+    subreddit_name: str
+    subreddit_id: str
+    subreddit_tag: str
+    subreddit_subscribers: int
     score: int
     comments_num: int
+    upvote_ratio: float
     is_video: bool
     media_only: bool
     over_18: bool
+    has_spoiler: bool
+    created_at: datetime
 
     def __convert_time(self, time_delta: timedelta):
         seconds = round(time_delta.total_seconds())
@@ -47,15 +114,21 @@ class Post:
                 return f"{seconds} seconds ago"
 
     def __str__(self) -> str:
-        from_time_stamp = datetime.fromtimestamp(self.created_at)
         over_18 = (
             ""
             if self.over_18 is False
             else Bold(f"🔞 Over 18 🔞: {self.over_18}\n\n").as_html()
         )
+        has_spoiler = (
+            ""
+            if self.has_spoiler is False
+            else Bold(f"🌫With spoiler🌫: {self.has_spoiler}\n\n").as_html()
+        )
         return (
             Bold(f"{self.post_type}🔥 from {self.subreddit.name}\n\n").as_html()
-            + f"{Bold('🔍 Title 🔍: ').as_html()}{TextLink(self.title, url=self.link).as_html()}\n\n"
+            + Bold("🔍 Title 🔍: ").as_html()
+            + TextLink(self.title, url=self.link).as_html()
+            + "\n\n"
             + Bold(f"✍️ Author ✍️: {self.author}\n\n").as_html()
             + Bold(f"💬 Comments Num 💬: {self.comments_num}\n\n").as_html()
             + Bold(f"⭐️ Score ⭐️: {self.score}\n\n").as_html()
@@ -63,10 +136,11 @@ class Post:
             + Bold(f"📱 Media Only 📱: {self.media_only}\n\n").as_html()
             + Bold(f"🎥 Video 🎥: {self.is_video}\n\n").as_html()
             + over_18
+            + has_spoiler
             + Bold(
-                "⏳Created at⏳: {created_at} ({time_ago})\n\n".format(
-                    created_at=from_time_stamp,
-                    time_ago=self.__convert_time(datetime.utcnow() - from_time_stamp),
+                "⏳ Created UTC ⏳: {created_at} ({time_ago})\n\n".format(
+                    created_at=str(self.created_at),
+                    time_ago=self.__convert_time(datetime.utcnow() - self.created_at),
                 )
             ).as_html()
             + HashTag(self.subreddit.tag).as_html()
